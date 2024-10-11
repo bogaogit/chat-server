a1ea02507b425346fda0a8ad8b386babc019c58a
Bo Gao <bogaogit@gmail.com>
2024-10-11 15:30:55 +1000

    C

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ dotenv.config();
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
-    cors: true
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST", "PUT"]
+    }
 });
 
 app.use(cors());
